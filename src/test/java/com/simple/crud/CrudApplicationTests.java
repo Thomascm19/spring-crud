5c38d01d596328551b7d38c1132b67ebe1fdf9f4
Thomas Caycedo Martinez <thomas.caycedo@pragma.com.co>
2022-10-25 16:29:18 -0500

    fix(CI) Fix CI config file

--- src/test/java/com/simple/crud/CrudApplicationTests.java
+++ src/test/java/com/simple/crud/CrudApplicationTests.java
@@ -3,7 +3,7 @@ package com.simple.crud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CrudApplication.class)
 class CrudApplicationTests {
 
 	@Test
